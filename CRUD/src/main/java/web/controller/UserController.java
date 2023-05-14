83b78038c975b31016e542d18d245e0cc6225a9e
IrinaMaslennikova <114755377+IrinaMaslennikova@users.noreply.github.com>
2023-05-14 19:26:16 +0700

    Add files via upload

---
+++ CRUD/src/main/java/web/controller/UserController.java
@@ -0,0 +1,64 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import web.dao.UserDAO;
+import web.dao.UserDAOImp;
+import web.model.User;
+import web.service.UserService;
+import web.service.UserServiceImp;
+
+import javax.transaction.Transactional;
+import java.awt.print.Book;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ModelAndView allUsers() {
+        List<User> users = userService.allUsers();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("users");
+        modelAndView.addObject("usersList", users);
+        return modelAndView;
+    }
+    @GetMapping("/addUser")
+    public String addUser(Model model) {
+        model.addAttribute("user",new User());
+        return "addUser";
+    }
+    @PostMapping(value = "/users")
+    public String create(@ModelAttribute("user") User user) {
+        userService.add(user);
+        return "redirect:/users";
+    }
+    @GetMapping("/edit/{id}")
+    public String update(@PathVariable(value = "id") int id,Model model){
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping (value = "/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.edit(user, id);
+        return "redirect:/users";
+    }
+    @DeleteMapping ("/{id}")
+    public String delete(@PathVariable (value = "id") int id) {
+        userService.delete(userService.getUserById(id));
+        return "redirect:/users";
+    }
+
+}
